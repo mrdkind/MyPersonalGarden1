eeea9553842a2de16e71f1abb80492abe6a24096
mrdkind <mrkind9@gmail.com>
2025-03-17 21:58:57 +0330

    Quartz sync: Mar 17, 2025, 9:58 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,38 +1,81 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'eledah/quartz_blog',
+        // from data-repo-id
+        repoId: 'R_kgDOLxbW_g',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOLxbW_s4ChRbJ',
+      }
+    }),
+  ],
+  footer: Component.Footer({
+    links: {
+      // "آپارات": "https://www.aparat.com/Crystalline",
+      // "گیت‌هاب": "https://github.com/eledah",
+    },
+  }),
+}
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-  Component.Breadcrumbs(),
-  Component.ArticleTitle(),
-  Component.ContentMeta(),
-  Component.TagList(),
-  ],
-  left: [
-  Component.DesktopOnly(Component.Graph()),
-  Component.DesktopOnly(Component.Backlinks()),
-  Component.MobileOnly(Component.PageTitle()),
-  Component.MobileOnly(Component.Darkmode()),
-  ],
-  right: [
-  Component.DesktopOnly(Component.PageTitle()),
-  Component.Search(),
-  Component.DesktopOnly(Component.Darkmode()),
-  Component.DesktopOnly(Component.Explorer()),
-  Component.DesktopOnly(Component.TableOfContents()),
-  Component.MobileOnly(Component.Backlinks()),
-  ],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
+  left: [
+    // Component.DesktopOnly(Component.Sidenotes()),
+    Component.DesktopOnly(Component.Graph()),
+    Component.DesktopOnly(Component.Backlinks()),
+
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
+    // Component.MobileOnly(Component.Search()),
+  ],
+  right: [
+    Component.DesktopOnly(Component.PageTitle()),
+    Component.DesktopOnly(Component.Darkmode()),
+    Component.DesktopOnly(Component.Search()),
+    // Component.DesktopOnly(Component.Explorer({
+    //   filterFn: (node) => {
+    //     // exclude files with the tag "explorerexclude"
+    //     return node.file?.frontmatter?.tags?.includes("explorerexclude") !== true
+    //   },
+    // })),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.Backlinks()),
+  ],
 }
- 
-// components for pages that display lists of pages  (e.g. tags or folders)
+
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
- beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-  Component.MobileOnly(Component.PageTitle()),
-  Component.MobileOnly(Component.Darkmode()),
-  ],
-  right: [
-  Component.DesktopOnly(Component.PageTitle()),
-  Component.Search(),
-  Component.DesktopOnly(Component.Darkmode()),
-  Component.DesktopOnly(Component.Explorer()),
-  ],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
+  ],
+  right: [
+    Component.DesktopOnly(Component.PageTitle()),
+    Component.Search(),
+    Component.DesktopOnly(Component.Darkmode()),
+    Component.DesktopOnly(Component.Explorer({
+      filterFn: (node) => {
+        // exclude files with the tag "explorerexclude"
+        return node.file?.frontmatter?.tags?.includes("explorerexclude") !== true
+      },
+    })),
+  ],
 }
